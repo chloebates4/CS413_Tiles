2d64d2a1debffd555c8565ac2726f8cd078f58b1
Samantha Muellner <shm53@nau.edu>
2019-10-27 12:08:08 -0700

    Update tiles.tsx

--- assets/tiles.tsx
+++ assets/tiles.tsx
@@ -1,4 +1,24 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.2" tiledversion="1.2.5" name="tiles" tilewidth="32" tileheight="32" spacing="4" margin="4" tilecount="4" columns="2">
  <image source="tiles.png" width="76" height="76"/>
+ <tile id="0">
+  <properties>
+   <property name="name" value="Lava"/>
+  </properties>
+ </tile>
+ <tile id="1">
+  <properties>
+   <property name="name" value="Purple"/>
+  </properties>
+ </tile>
+ <tile id="2">
+  <properties>
+   <property name="name" value="Grass"/>
+  </properties>
+ </tile>
+ <tile id="3">
+  <properties>
+   <property name="name" value="Bricks"/>
+  </properties>
+ </tile>
 </tileset>
