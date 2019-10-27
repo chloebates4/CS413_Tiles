fa80de8cad848cea6a2d84dbe394b95bffddf9b0
Samantha Muellner <shm53@nau.edu>
2019-10-27 11:42:03 -0700

    New and Improved Json Map

---
+++ assets/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.5" name="tiles" tilewidth="32" tileheight="32" spacing="4" margin="4" tilecount="4" columns="2">
+ <image source="tiles.png" width="76" height="76"/>
+</tileset>
